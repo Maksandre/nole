ea435d29c61d12ed0d301899a5b64c269c2a6941
Pavel <Mashino@mail.ru>
2024-09-10 22:33:05 +0400

    docs: add readme and fix scripts (#7)
    Co-authored-by: Paul M <paulmapaulma3@gmail.com>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # nil apps
+
+## Installation
+
+1. Create a `.env` file based on `.env.example` and configure the necessary environment variables.
+
+2. Install dependencies and compile the contracts:
+
+```sh
+yarn
+yarn compile
+```
+
+## Running Tests
+
+To run all tests, execute:
+
+```sh
+yarn test
+```
+
+Alternatively, you can debug the tests in Visual Studio Code using the `TEST` script found in the "Run and Debug" section.
--- src/client/utils/config.ts
+++ src/client/utils/config.ts
@@ -3,10 +3,9 @@ import * as dotenv from "dotenv";
 dotenv.config();
 
 const getConfig = () => {
-  const { NIL_RPC_ENDPOINT, PRIVATE_KEY, WALLET_ADDRESS, XWALLET_ADDRESS } =
-    process.env;
+  const { NIL_RPC_ENDPOINT, PRIVATE_KEY, WALLET_ADDRESS } = process.env;
 
-  if (!NIL_RPC_ENDPOINT || !PRIVATE_KEY || !WALLET_ADDRESS || !XWALLET_ADDRESS)
+  if (!NIL_RPC_ENDPOINT || !PRIVATE_KEY || !WALLET_ADDRESS)
     throw Error("Did you forget to set .env?");
 
   const shardId = 1;
@@ -15,7 +14,6 @@ const getConfig = () => {
     rpc: NIL_RPC_ENDPOINT,
     signerPrivateKey: PRIVATE_KEY as Hex,
     walletAddress: WALLET_ADDRESS.toLowerCase() as Hex,
-    xWalletAddress: XWALLET_ADDRESS.toLowerCase() as Hex,
     shardId,
   };
 };
